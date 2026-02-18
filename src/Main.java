cd3d434bbbcd0d86bd361482b9d9da1ad9639728
yuva1355 <sambangiyuvakishore@gmail.com>
2026-02-18 15:40:59 +0530

    Update Main.java for UC1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-git checkout main//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome App   ");
+        System.out.println("=================================");
+        System.out.println("This application checks whether");
+        System.out.println("a word is a palindrome or not.");
+
     }
 }
