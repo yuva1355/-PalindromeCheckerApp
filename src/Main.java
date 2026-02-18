ec856cb899bbc9ef90465cb8ba4a08e03b49efde
yuva1355 <sambangiyuvakishore@gmail.com>
2026-02-18 15:21:30 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+git checkout main//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
