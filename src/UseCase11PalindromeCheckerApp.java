97033b5473fa0ed8a511af82839f50ed91622cdd
yuva 1355 <sambangiyuvakishore@gmail.com>
2026-02-25 14:59:19 +0530

    UC11: Object-Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,43 @@
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input) {
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Palindrome Checker - UC11 (OOP Service)");
+        System.out.println("--------------------------------------------");
+
+        String input = "Madam";
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program finished.");
+    }
+}
